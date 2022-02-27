099c92bcb95edbc0787b68ab65465cabc2fb14d6
Unoka Ruth <33810711+Ruthy4@users.noreply.github.com>
2022-02-27 18:51:39 +0100

    extract file to package

--- app/src/main/java/com/example/lawpavillionandroidassessment/ui/indexanddigest/IndexAndDigestFragment.kt
+++ app/src/main/java/com/example/lawpavillionandroidassessment/ui/indexanddigest/IndexAndDigestFragment.kt
@@ -1,10 +1,11 @@
-package com.example.lawpavillionandroidassessment
+package com.example.lawpavillionandroidassessment.ui.indexanddigest
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.lawpavillionandroidassessment.R
 
 class IndexAndDigestFragment : Fragment() {
 
